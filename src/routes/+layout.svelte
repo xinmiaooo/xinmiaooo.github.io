d5072e4648a976cc070f44eb07a96a0d93de57ed
Qu Xinmiao <83005951+xinmiaooo@users.noreply.github.com>
2026-04-01 18:54:46 +0900

    favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.png';
 	import * as NavigationMenu from '$lib/components/ui/navigation-menu/index.js';
 
 	let { children } = $props();
